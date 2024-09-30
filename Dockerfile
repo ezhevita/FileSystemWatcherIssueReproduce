cfb6f0f253349f5399b7397dc31d97f69e902dfd
Vita Chumakova <me@ezhevita.dev>
2024-09-30 15:01:56 +0400

    Update to latest .NET SDKs, remove multi-thread version

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,11 @@
-FROM mcr.microsoft.com/dotnet/sdk:6.0 AS sdk-6
-FROM mcr.microsoft.com/dotnet/sdk:7.0 AS main
-COPY --from=sdk-6 /usr/share/dotnet/ /usr/share/dotnet/
+FROM mcr.microsoft.com/dotnet/sdk:8.0 AS stable
+FROM mcr.microsoft.com/dotnet/sdk:9.0 AS prerelease
+COPY --from=stable /usr/share/dotnet/ /usr/share/dotnet/
 COPY . /app
 ENV BASE_PATH="/test"
 RUN mkdir /test
 RUN /app/bootstrap.sh /test
 
 WORKDIR /app/FileSystemWatcherIssueReproduce
-RUN dotnet publish -c Release -f net7.0 -o out
-ENTRYPOINT /app/FileSystemWatcherIssueReproduce/out/FileSystemWatcherIssueReproduce
+RUN dotnet publish -c Release -f net8.0 -o out
+ENTRYPOINT ["/app/FileSystemWatcherIssueReproduce/out/FileSystemWatcherIssueReproduce"]
--- FileSystemWatcherIssueReproduce/FileSystemWatcherBenchmark.cs
+++ FileSystemWatcherIssueReproduce/FileSystemWatcherBenchmark.cs
@@ -1,53 +1,36 @@
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Jobs;
 
 namespace FileSystemWatcherIssueReproduce;
 
-[SimpleJob(RuntimeMoniker.Net60)]
-[SimpleJob(RuntimeMoniker.Net70)]
+[SimpleJob(RuntimeMoniker.Net80)]
+[SimpleJob(RuntimeMoniker.Net90)]
 public class FileSystemWatcherBenchmark
 {
 	[Params(10)]
+	[SuppressMessage("ReSharper", "UnusedAutoPropertyAccessor.Global", Justification = "Set implicitly by BenchmarkDotNet")]
 	public int FileCount { get; set; }
 
-	[Params(4)]
-	public int ThreadCount { get; set; }
-
-	public string TestPath { get; set; } = null!;
+	private string _testPath = null!;
 
 	[GlobalSetup]
 	public void Setup()
 	{
-		TestPath = Environment.GetEnvironmentVariable("BASE_PATH")!; // replace this with custom path if needed
+		// replace this with custom path if needed
+		_testPath = Environment.GetEnvironmentVariable("BASE_PATH") ?? throw new ArgumentNullException();
 	}
 
 	[Benchmark]
-	public void RunSingleThreaded()
+	public void Run()
 	{
 		for (var i = 0; i < FileCount; i++)
 		{
-			using var watcher = new FileSystemWatcher(Path.Join(TestPath)) {Filter = $"file{i:D2}.txt"};
+			using var watcher = new FileSystemWatcher(Path.Join(_testPath));
+			watcher.Filter = $"file{i:D2}.txt";
 			watcher.EnableRaisingEvents = true;
-		}
-	}
-
-	[Benchmark]
-	public void RunMultiThreaded()
-	{
-		var threads = new Thread[ThreadCount];
-		for (var i = 0; i < threads.Length; i++)
-		{
-			threads[i] = new Thread(RunSingleThreaded);
-		}
-
-		foreach (var thread in threads)
-		{
-			thread.Start();
-		}
-
-		foreach (var thread in threads)
-		{
-			thread.Join();
 		}
 	}
 }
